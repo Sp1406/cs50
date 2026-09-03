f24602a49c2e192a106ed08a559c03e4ab6c5f98
unknown <sankalppandey1406@gmail.com>
2026-09-03 17:47:21 +0530

    mario2.c

---
+++ Week1 - C/section/mario2.c
@@ -0,0 +1,26 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int bricks);
+
+int main(void) {
+    // Prompt user for height (int)
+    int height;
+    do {
+        height = get_int("Height: ");
+    } while (height < 1);
+
+
+    // Print pyramid of that height
+    for (int i = 0; i < height; i++) {
+        print_row(i+1);
+    }
+}
+
+// Given a number of bricks, print that number of bricks
+void print_row(int bricks) {
+    for(int i = 0; i < bricks; i++) {
+        printf("#");
+    }
+    printf("\n");
+}
